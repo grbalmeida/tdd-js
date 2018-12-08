b65736a69099b9ea6013186ba9023f6b1ba6b547
gilvan <g.r.almeida@live.com>
2018-12-08 05:56:14 -0200

    Adding .deep.equal

--- src/filter/recursive-filter.test.js
+++ src/filter/recursive-filter.test.js
@@ -19,15 +19,15 @@ it('recursiveFilter(htmlTags, regex) should return ["<h1>"]', () => {
 
 it('recursiveFilter(dates, regex) should return ["10/10/2010"]', () => {
     const regex = /^[0-3]{1}[0-9]{1}\/[0-1]{1}[0-2]\/\d{4}$/gmi
-    expect(recursiveFilter(dates, date => date.match(regex)))
+    expect(recursiveFilter(dates, date => date.match(regex))).to.be.deep.equal(['10/10/2010'])
 })
 
 it('recursiveFilter(cpfs, regex) should return ["123.123.123-12"]', () => {
     const regex = /^(\d{3}\.){2}\d{3}\-\d{2}$/gmi
-    expect(recursiveFilter(cpfs, cpf => cpf.match(regex)))
+    expect(recursiveFilter(cpfs, cpf => cpf.match(regex))).to.be.deep.equal(['123.123.123-12'])
 })
 
 it('recursiveFilter(urls, regex) should return ["http://google.com"]', () => {
     const regex = /^https?\:\/\/[a-z0-9]+\.[a-z0-9]+(\.[a-z0-9]+)?$/gmi
-    expect(recursiveFilter(urls, url => url.match(regex)))
+    expect(recursiveFilter(urls, url => url.match(regex))).to.be.deep.equal(['http://google.com'])
 })
